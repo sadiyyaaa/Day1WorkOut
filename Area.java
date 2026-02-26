a76193bea008b202729b7f984b23182e507774cf
sadiyyaaa <sadiyakotttur@gmail.com>
2026-02-26 12:18:53 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,11 @@
+public class Area {
+    public static void main(String[] args) {
+        double radius =5.0;
+        double area = Math.PI *radius * radius;
+        System.out.println("The area of circle = " + area);
+    }
+    
+}
+
+    
+
