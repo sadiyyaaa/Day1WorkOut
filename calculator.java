c560c46e758cdb345c519850aef1284ada9721c7
sadiyyaaa <sadiyakotttur@gmail.com>
2026-02-26 12:21:00 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,15 @@
+public class calculator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("sum"+sum);
+        int sub=a-b;
+        System.out.println("sub"+sub);
+        int multi=a*b;
+        System.out.println("multi"+multi);
+        int divisor=a/b;
+        System.out.println("divisor"+divisor);
+    }
+    
+}
